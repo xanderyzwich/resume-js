67c8ecc6a673610f570707e55f8fba8360190fa4
5053569 <corey.mccarty.osv@fedex.com>
2019-11-22 12:56:20 -0600

    data cleaning

--- src/data.js
+++ src/data.js
@@ -1,80 +1,79 @@
 let data = {
-  "Name": {
-    "first": "Corey",
-    "last": "McCarty"
+  'Name': {
+    'first': 'Corey',
+    'last': 'McCarty'
   },
-  "Contact": {
-    "Phone": "(901) 832-7968",
-    "Email": "Corey.D.McCarty@gmail.com",
-    "Profile": "linkedin.com/in/coreydmccarty",
-    "Repository": "github.com/xanderyzwich"
+  'Contact': {
+    'Phone': '(901) 832-7968',
+    'Email': 'Corey.D.McCarty@gmail.com',
+    'Profile': 'linkedin.com/in/coreydmccarty',
+    'Repository': 'github.com/xanderyzwich'
   },
-  "Skills": {
-    "Languages": {
-      "subtitile": "Java, Python, SQL, JavaScript, ASM, C/C++, Bash, XML, YML"},
-    "Deployments": [ "Pivotal Cloud Foundry, RedHat, Tomcat, Springboot, Oracle DB, Datastax/Cassandra, JAMstack, Android, Arduino" ],
-    "Version/Build Tools": [ "Git, SVN, Ant, Gradle, Jenkins, TravisCI, Nexus" ],
-    "Frameworks": [ "Spring, Slf4j, Log4j, LogBack, Django" ],
-    "Development Tools": [ "JetBrains, Eclipse, Oracle SQL Developer, HPE Agile Manager, SoapUI" ]
+  'Skills': {
+    'Languages': ['Java, Python, SQL, JavaScript, ASM, C/C++, Bash, XML, YML'],
+    'Deployments': [ 'Pivotal Cloud Foundry, RedHat, Tomcat, Springboot, Oracle DB, Datastax/Cassandra, JAMstack, Android, Arduino' ],
+    'Version/Build Tools': [ 'Git, SVN, Ant, Gradle, Jenkins, TravisCI, Nexus' ],
+    'Frameworks': [ 'Spring, Slf4j, Log4j, LogBack, Django' ],
+    'Development Tools': [ 'JetBrains, Eclipse, Oracle SQL Developer, HPE Agile Manager, SoapUI' ]
   },
-  "Projects": {
-    "XML data warehouse": {
-      "Role": "Application Development Lifecycle",
-      "Stack": "Java, Spring, Oracle DB, SOAP, XML, Cassandra/Datastax, Tomcat, Bash, JMS, JMX"
+  'Projects': {
+    'XML data warehouse': {
+      'Role': 'Application Development Lifecycle',
+      'Stack': 'Java, Spring, Oracle DB, SOAP, XML, Cassandra/Datastax, Tomcat, Bash, JMS, JMX'
     },
-    "Authorization for Fraud Prevention": {
-      "Role": "Architect & Development",
-      "Stack": "Java, Spring, SOAP, Weblogic, Bash"
+    'Authorization for Fraud Prevention': {
+      'Role': 'Architect & Development',
+      'Stack': 'Java, Spring, SOAP, Weblogic, Bash'
     },
-    "Custom Sensor Array and Driver": {
-      "Role": "Project Management & Development",
-      "Stack": "Arduino, Java"
+    'Custom Sensor Array and Driver': {
+      'Role': 'Project Management & Development',
+      'Stack': 'Arduino, Java'
     },
-    "Simulate Electro-Magnetic Fields": {
-      "Role": "Application Development",
-      "Stack": "Python, NumPy, SciPy, MatPlotLib, GNU Octave"
+    'Simulate Electro-Magnetic Fields': {
+      'Role': 'Application Development',
+      'Stack': 'Python, NumPy, SciPy, MatPlotLib, GNU Octave'
     },
-    "Distracted Driving Remedy": {
-      "Role": "Research, Development, and Analysis",
-      "Stack": "Android, Java"
+    'Distracted Driving Remedy': {
+      'Role': 'Research, Development, and Analysis',
+      'Stack': 'Android, Java'
     },
-    "Robotics Competition": {
-      "Role": "Project Manager",
-      "Stack": "Arduino, Java, custom hardware"
+    'Robotics Competition': {
+      'Role': 'Project Manager',
+      'Stack': 'Arduino, Java, custom hardware'
     },
-    "Matrix Calculator": {
-      "Role": "Application Development",
-      "Stack": "C++"
+    'Matrix Calculator': {
+      'Role': 'Application Development',
+      'Stack': 'C++'
     }
   },
-  "Education": {
-    "School": "University of Memphis - Herff College of Engineering",
-    "Degree": "B.S. in Computer Engineering",
-    "Graduation Date": "December 2013",
-    "Description": "Study focused on hands on hardware and software system design of analog and digital circuits, microprocessors, MIPS and Motorola 68HC11 assembly, Arduino, C, Java, Python, and MATLAB. Mathematics secondary included probability, statics, caclulus, and differential equations."
+  'Education': {
+    'School': 'University of Memphis - Herff College of Engineering',
+    'Degree': 'B.S. in Computer Engineering',
+    'Graduation Date': 'December 2013',
+    'Description': 'Study focused on hands on hardware and software system design of analog and digital circuits, microprocessors, MIPS and Motorola 68HC11 assembly, Arduino, C, Java, Python, and MATLAB. Mathematics secondary included probability, statics, caclulus, and differential equations.'
   },
-  "Experience":{
-    "Atos|Syntel - Software Engineer": {
-      "Dates": "September 2014 - Present",
-      "Achievements": [
-        "Scripting with Bash and Python to provide on the go analysis of team activities from code base and data extracts",
-        "Make legacy java application cloud native using Gradle, SpringBoot, Splunk, and App Dynamics ",
-        "Spearhead learning of GitLab repository usage and Jenkins pipeline tools to maintain 15 applications CI/CD process",
-        "Apply database design principles to data tables with up to 20 billion entries for reduction of storage footprint and query times while increasing uptime",
-        "Design and implement code style standards and patterns to increase understanding and expedite work by onsite and offshore development teams",
-        "Challenge standards and improve efficiency in both code and process for betterment of product, team, and self"
+  'Experience':{
+    'Atos|Syntel - Software Engineer': {
+      'Dates': 'September 2014 - Present',
+      'Achievements': [
+        'Scripting with Bash and Python to provide on the go analysis of team activities from code base and data extracts',
+        'Make legacy java application cloud native using Gradle, SpringBoot, Splunk, and App Dynamics ',
+        'Spearhead learning of GitLab repository usage and Jenkins pipeline tools to maintain 15 applications CI/CD process',
+        'Apply database design principles to data tables with up to 20 billion entries for reduction of storage footprint and query times while increasing uptime',
+        'Design and implement code style standards and patterns to increase understanding and expedite work by onsite and offshore development teams',
+        'Challenge standards and improve efficiency in both code and process for betterment of product, team, and self'
       ]
     },
-    "Various Positions": [
-      "Restaurant Server/Bartender - 6 years",
-      "Printing and Bindery Production - 1 year",
-      "Oil Change Technician/Manager - 3 years",
-      "Fast Food Service / Team Lead - 2 years"
+    'Various Positions': [
+      'Restaurant Server/Bartender - 6 years',
+      'Printing and Bindery Production - 1 year',
+      'Oil Change Technician/Manager - 3 years',
+      'Fast Food Service / Team Lead - 2 years'
     ],
-    "Volunteer Work": [
-      "Code Support",
-      "Knights of Columbus",
-      "GiveCamp"
+    'Volunteer Work': [
+      'Code Support',
+      'Knights of Columbus',
+      'GiveCamp'
     ]
   }
 };
