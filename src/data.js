a9e3b968488d6066aeec13dd121d2884156c76bc
5053569 <corey.mccarty.osv@fedex.com>
2019-11-25 15:06:34 -0600

    updated data

--- src/data.js
+++ src/data.js
@@ -20,24 +20,24 @@ let data = {
     }
   },
   'Skills': {
-    'Languages': ['Java, Python, SQL, JavaScript, Bash, XML, YML'],
-    'Deployments': ['Pivotal Cloud Foundry, RedHat, Tomcat, Springboot, Oracle DB, Datastax/Cassandra, JAMstack, Android, Arduino'],
-    'Version/Build Tools': ['Git, SVN, Ant, Gradle, Jenkins, TravisCI, Nexus'],
+    'Languages': ['Java, Python, SQL, JavaScript, Bash'],
+    'Deployments': ['Pivotal Cloud Foundry, RedHat, Tomcat, Springboot, Oracle DB, Datastax/Cassandra NoSQL, JamStack, Android, Arduino'],
+    'DevOps': ['Git, SVN, Ant, Gradle, Jenkins, TravisCI, Nexus'],
     'Frameworks': ['Spring, Slf4j, Log4j, LogBack, Django'],
-    'Development Tools': ['JetBrains, Eclipse, Oracle SQL Developer, HPE Agile Manager, SoapUI']
+    'Development/Testing Tools': ['JetBrains, Eclipse, Oracle SQL Developer, HPE Agile Manager, SoapUI']
   },
   'Projects': {
-  	'Customer Hieracharchy Management': {
+  	'Customer Hierarchy Management': {
   		'Application Modernization': ['Java, Springboot, Pivotal Cloud Foundry, Jenkins, GitLab, Oracle DB, JMS, Soap']
-  	}
+  	},
     'XML data warehouse': {
       'Application Development Lifecycle': ['Java, Spring, Oracle DB, SOAP, XML, Cassandra/Datastax, Tomcat, Bash, JMS, JMX']
     },
     'Authorization for Fraud Prevention': {
       'Architect & Development': ['Java, Spring, SOAP, Weblogic, Bash']
     },
-    'Custom Sensor Array and Driver': {
-      'Project Management & Development': ['Arduino, Java']
+    'Custom Sensor Array and Drivers': {
+      'Project Management & Development': ['Arduino, Java, custom hardware']
     },
     'Simulate Electro-Magnetic Fields': {
       'Application Development': ['Python, NumPy, SciPy, MatPlotLib, GNU Octave']
@@ -54,18 +54,18 @@ let data = {
   },
   'University of Memphis': {
     'Degree': 'B.S. in Computer Engineering - December 2013',
-    'Description': 'Study focused on hands on hardware and software system design of analog and digital circuits, microprocessors, MIPS and Motorola 68HC11 assembly, Arduino, C, Java, Python, and MATLAB. Mathematics secondary included probability, statics, caclulus, and differential equations.'
+    'Description': 'Hands on Study of software and hardware system design of analog and digital circuits, microprocessors, MIPS and Motorola 68HC11 assembly, Arduino, C, Java, Python, and MATLAB. Mathematics secondary included probability, statics, caclulus, and differential equations.'
   },
   'Experience':{
     'Atos|Syntel - Software Engineer': {
       'Dates': 'September 2014 - Present',
       'Achievements': [
-        'Scripting with Bash and Python to provide on the go analysis of team activities from code base and data extracts',
-        'Make legacy java application cloud native using Gradle, SpringBoot, Splunk, and App Dynamics ',
-        'Spearhead learning of GitLab repository usage and Jenkins pipeline tools to maintain 15 applications CI/CD process',
-        'Apply database design principles to data tables with up to 20 billion entries for reduction of storage footprint and query times while increasing uptime',
-        'Design and implement code style standards and patterns to increase understanding and expedite work by onsite and offshore development teams',
-        'Challenge standards and improve efficiency in both code and process for betterment of product, team, and self'
+        'Analyze team activities from code base and data extracts with Bash and Python',
+        'Modernize legacy java applications to cloud native using Gradle, SpringBoot, Splunk, and App Dynamics',
+        'Lead DevOps drive with GitLab and Jenkins pipeline tools to maintain 16 applications CI/CD process',
+        'Manage 30TB of databases\' design to reduce storage footprint and query times while maximizing uptime',
+        'Standardize code style and patterns to increase understanding and expedite work by onsite and offshore development teams',
+        'Drive efficiency and standards evolution in both code and process for betterment of product, team, and self'
       ]
     },
     'Various Positions': [
